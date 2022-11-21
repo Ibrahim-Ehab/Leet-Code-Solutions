b31fa47379a78bda4ac3aa216eb4952667bbccda
Ibrahim-Ehab <62462167+Ibrahim-Ehab@users.noreply.github.com>
2022-11-21 10:25:42 +0200

    Time: 19 ms (42.90%), Space: 10.1 MB (29.48%) - LeetHub

--- 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.cpp
+++ 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
+        // define limit for loops and create the max skyline
         int nn=grid.size();
         int ans=0;
+        // create vectors for rows and columns which contains zeros for grid size dimension
         vector<int>row(nn,0),col(nn,0);
+        //make new grid with updated hights
         for(int r=0;r<nn;r++){
             for (int c=0;c<nn;c++){
                 row[r]=max(row[r],grid[r][c]);
                 col[c]=max(col[c],grid[r][c]);
             }
         }
+        //calculate the values which we added to old hights 
         for(int r=0;r<nn;r++){
             for (int c=0;c<nn;c++){
                 ans+=(min(row[r],col[c])-grid[r][c]);
