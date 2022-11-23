4bc0a2ac0ff677ebcc84605528d62397b527b3c6
Ibrahim-Ehab <62462167+Ibrahim-Ehab@users.noreply.github.com>
2022-11-23 13:36:09 +0200

    Time: 86 ms (69.20%), Space: 13.9 MB (67.99%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
@@ -1,9 +1,36 @@
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
         nums.sort()
-        ans=[]
-        for i in range (len(nums)):
-            if nums[i]==target:
-                ans.append(i)
-        return ans
         
+        def bin_search(arr, t, lower=True):
+            left, right = 0, len(arr) -1 
+            
+            while left <= right:
+
+                mid = (left + right) // 2
+
+                if arr[mid] == t:
+                    if lower:
+                        if mid == left or nums[mid] != arr[mid - 1]:
+                            return mid
+                        else:
+                            right = mid - 1
+                    else:
+                        if mid == right or arr[mid] != arr[mid + 1]:
+                            return mid
+                        else:
+                            left = mid + 1
+                        
+                elif arr[mid] > t:
+                    right = mid - 1 
+                elif nums[mid] < t:
+                    left = mid + 1
+            
+            return -1
+        
+        start, end = bin_search(nums, target), bin_search(nums, target, False)
+        
+        if start < 0 and end < 0:
+            return []
+        
+        return list(range(start, end + 1))
